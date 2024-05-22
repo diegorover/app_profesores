8eb817f1004351bd8f534714e95146313013a8b4
diegorover <155965519+diegorover@users.noreply.github.com>
2024-05-22 10:57:08 +0200

    Nuevas modificaciones del formulario

---
+++ lib/firebase_service_matematicas.dart
@@ -0,0 +1,47 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+// Función para obtener las preguntas del documento "Matematicas"
+Future<List<String>> getPreguntasMatematicas() async {
+  final docRef = FirebaseFirestore.instance.collection('profesores').doc('matematicas');
+  final docSnapshot = await docRef.get();
+
+  if (docSnapshot.exists) {
+    final data = docSnapshot.data();
+    if (data != null && data.containsKey('Preguntas')) {
+      List<String> preguntas = List<String>.from(data['Preguntas']);
+      // Asegúrate de que solo retornamos 5 preguntas
+      return preguntas.length > 5 ? preguntas.sublist(0, 5) : preguntas;
+    }
+  }
+  return [];
+}
+
+// Función para guardar las respuestas en Firestore
+Future<void> saveRespuestasMatematicas(Map<String, String> respuestas) async {
+  final docRef = FirebaseFirestore.instance.collection('profesores').doc('Respuestas_Matematicas');
+  final docSnapshot = await docRef.get();
+
+  int newFieldNumber = 1;
+  if (docSnapshot.exists) {
+    final data = docSnapshot.data();
+    if (data != null) {
+      // Encuentra el mayor número actual de 'Respuestas'
+      for (var key in data.keys) {
+        if (key.startsWith('Respuestas')) {
+          final number = int.tryParse(key.replaceFirst('Respuestas', ''));
+          if (number != null && number >= newFieldNumber) {
+            newFieldNumber = number + 1;
+          }
+        }
+      }
+    }
+  }
+
+  // Nombre del nuevo campo de respuestas
+  final newFieldName = 'Respuestas$newFieldNumber';
+
+  // Actualiza el documento con el nuevo campo de respuestas
+  await docRef.update({
+    newFieldName: respuestas,
+  });
+}
--- lib/inicio.dart
+++ lib/inicio.dart
@@ -1,37 +1,85 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'firebase_service_matematicas.dart';
+import 'main.dart';
 import 'firebase_service_lengua.dart';
-import 'inicio.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const MyApp());
+class Inicio extends StatefulWidget {
+  const Inicio({super.key});
+
+  @override
+  State<Inicio> createState() => _InicioState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _InicioState extends State<Inicio> {
+  final TextEditingController _codeController = TextEditingController();
+
+  void _navigateToHome() {
+    if (_codeController.text == '1') {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const Home()),
+      );
+    } else if (_codeController.text == '2') {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const HomeMatematicas()),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Código incorrecto')),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Material App',
-      home: Inicio(), // Cambia la pantalla inicial a Inicio
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ingresa el código del cuestionario'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Ingresa el código del cuestionario',
+              style: TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              controller: _codeController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Código',
+              ),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: _navigateToHome,
+              child: const Text('Ingresar'),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    _codeController.dispose();
+    super.dispose();
   }
 }
 
-class Home extends StatefulWidget {
-  const Home({super.key});
+class HomeMatematicas extends StatefulWidget {
+  const HomeMatematicas({super.key});
 
   @override
-  State<Home> createState() => _HomeState();
+  State<HomeMatematicas> createState() => _HomeMatematicasState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeMatematicasState extends State<HomeMatematicas> {
   final Map<String, TextEditingController> _controllers = {};
 
   Future<void> _submitRespuestas() async {
@@ -51,7 +99,7 @@ class _HomeState extends State<Home> {
 
     final respuestas = _controllers.map((key, value) => MapEntry(key, value.text));
     try {
-      await saveRespuestas(respuestas);
+      await saveRespuestasMatematicas(respuestas);
       ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Respuestas guardadas exitosamente')));
       Navigator.pushReplacement(
         context,
@@ -70,7 +118,7 @@ class _HomeState extends State<Home> {
         automaticallyImplyLeading: false, // Deshabilitar el botón de regreso
       ),
       body: FutureBuilder<List<String>>(
-        future: getPreguntas(),
+        future: getPreguntasMatematicas(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
