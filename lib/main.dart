190a31dd2c49d31c8a35095b4a830febf72fcbbc
diegorover <155965519+diegorover@users.noreply.github.com>
2024-05-23 11:37:36 +0200

    Funcionamiento anterior

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'firebase_service_lengua.dart';
-import 'firebase_service_matematicas.dart';
 import 'inicio.dart';
-import 'auth.dart';
-
-
-import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,20 +17,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Material App',
-      home: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const CircularProgressIndicator();
-          }
-          if (snapshot.hasData) {
-            return const Inicio();
-          }
-          return const SignInPage();
-        },
-      ),
+      home: Inicio(), // Cambia la pantalla inicial a Inicio
     );
   }
 }
@@ -51,15 +35,6 @@ class _HomeState extends State<Home> {
   final Map<String, TextEditingController> _controllers = {};
 
   Future<void> _submitRespuestas() async {
-    User? user = FirebaseAuth.instance.currentUser;
-    if (user == null) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Error: usuario no autenticado')),
-      );
-      return;
-    }
-    String userId = user.uid; // Obtener el ID del usuario autenticado
-
     bool allFilled = true;
     _controllers.forEach((key, controller) {
       if (controller.text.isEmpty) {
@@ -76,14 +51,12 @@ class _HomeState extends State<Home> {
 
     final respuestas = _controllers.map((key, value) => MapEntry(key, value.text));
     try {
-      await saveRespuestas(respuestas, userId);
+      await saveRespuestas(respuestas);
       ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Respuestas guardadas exitosamente')));
-      if (mounted) {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => const Inicio()), // Redirigir a la página de inicio
-        );
-      }
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const Inicio()), // Redirigir a la página de inicio
+      );
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Error al guardar respuestas: $e')));
     }
@@ -98,120 +71,6 @@ class _HomeState extends State<Home> {
       ),
       body: FutureBuilder<List<String>>(
         future: getPreguntas(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(child: Text('No hay preguntas disponibles.'));
-          } else {
-            final preguntas = snapshot.data!;
-            for (var pregunta in preguntas) {
-              _controllers[pregunta] = TextEditingController();
-            }
-            return Column(
-              children: [
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: preguntas.length,
-                    itemBuilder: (context, index) {
-                      final pregunta = preguntas[index];
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(pregunta),
-                            TextField(
-                              controller: _controllers[pregunta],
-                              decoration: const InputDecoration(
-                                hintText: 'Escribe tu respuesta aquí',
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                  ),
-                ),
-                ElevatedButton(
-                  onPressed: _submitRespuestas,
-                  child: const Text('Enviar respuestas'),
-                ),
-              ],
-            );
-          }
-        },
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    _controllers.forEach((key, controller) => controller.dispose());
-    super.dispose();
-  }
-}
-
-class HomeMatematicas extends StatefulWidget {
-  const HomeMatematicas({super.key});
-
-  @override
-  State<HomeMatematicas> createState() => _HomeMatematicasState();
-}
-
-class _HomeMatematicasState extends State<HomeMatematicas> {
-  final Map<String, TextEditingController> _controllers = {};
-
-  Future<void> _submitRespuestas() async {
-    User? user = FirebaseAuth.instance.currentUser;
-    if (user == null) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Error: usuario no autenticado')),
-      );
-      return;
-    }
-    String userId = user.uid; // Obtener el ID del usuario autenticado
-
-    bool allFilled = true;
-    _controllers.forEach((key, controller) {
-      if (controller.text.isEmpty) {
-        allFilled = false;
-      }
-    });
-
-    if (!allFilled) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Por favor, responde todas las preguntas.')),
-      );
-      return;
-    }
-
-    final respuestas = _controllers.map((key, value) => MapEntry(key, value.text));
-    try {
-      await saveRespuestasMatematicas(respuestas, userId);
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Respuestas guardadas exitosamente')));
-      if (mounted) {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => const Inicio()), // Redirigir a la página de inicio
-        );
-      }
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Error al guardar respuestas: $e')));
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Material App Bar'),
-        automaticallyImplyLeading: false, // Deshabilitar el botón de regreso
-      ),
-      body: FutureBuilder<List<String>>(
-        future: getPreguntasMatematicas(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
